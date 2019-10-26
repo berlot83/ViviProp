fedbfb4cac4e5ef1246f17baa47e5c2de68870d6
berlot83 <berlot83@yahoo.com.ar>
2019-10-26 18:20:56 -0300

    problemas con heroku web

--- src/main/java/com/viviprop/config/PageConfig.java
+++ src/main/java/com/viviprop/config/PageConfig.java
@@ -3,53 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.molokotech.config;
-
-import java.util.Locale;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+package com.viviprop.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-@ComponentScan("com.molokotech.controllers")
+@ComponentScan("com.viviprop.controllers")
 public class PageConfig implements WebMvcConfigurer {
-
-	@Autowired
-	ApplicationContext applicationContext;
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/robots.txt").setViewName("robots.txt");
-		registry.addViewController("/sitemap.xml").setViewName("sitemap.xml");
-		registry.addViewController("/BingSiteAuth.xml").setViewName("BingSiteAuth.xml");
-	}
-	
-	/* Thymeleaf local config start / Set  default lang to English, deactivate, but active if needed */
-	@Bean
-	public SessionLocaleResolver localeResolver() {
-	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setDefaultLocale(Locale.forLanguageTag("es"));
-	    return slr;
-	}
-	
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-	    lci.setParamName("lang");
-	    return lci;
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
-	}
-	/* Thymeleaf local config end */
 
 }
